cc36cce3523d0bd138c097d2228ba0cdcfffd482
daehong ko <steve.ko.dh@gmail.com>
2019-08-22 01:03:13 +0800

    #1 Hello World
    * Storyboard에 Label 추가 후 'Hello World' 입력
    * 'Hello World' Label constraints 적용
    * ViewController 에 IBOutlet 연결하여 Label text를 programmatically하게 수정

--- iMDNotepad/ViewController.swift
+++ iMDNotepad/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSample: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelSample.text = "Hello World !!"
     }
 
 
